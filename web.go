5c5782eda6cdeaaa9bb0f6ac31498b2c66f58332
AnatoleLucet <anatole.lucet@proton.me>
2026-01-03 19:26:53 +0100

    feat(web renderer)!: new app api

--- web.go
+++ web.go
@@ -3,24 +3,59 @@
 package web
 
 import (
+	"errors"
+
 	"github.com/AnatoleLucet/loom"
 	"github.com/AnatoleLucet/loom-web/internal"
 	"github.com/AnatoleLucet/loom/signals"
 )
 
-func Render(parent string, node loom.Node) (*signals.Owner, error) {
-	o := signals.NewOwner()
-	o.OnError(func(err any) {
-		internal.ConsoleError("Recovered from panic:", err)
-	})
+type App struct {
+	running bool
 
-	err := o.Run(func() error {
-		return internal.RenderNodes(loom.NewRenderContext(), internal.Doc().Call("querySelector", parent), node)
-	})
-	if err != nil {
-		o.Dispose()
-		return nil, err
+	owner *signals.Owner
+}
+
+func NewApp() *App {
+	return &App{
+		running: false,
+		owner:   signals.NewOwner(),
+	}
+}
+
+func (a *App) Run(parent string, node loom.Node) <-chan any {
+	errc := make(chan any)
+	if a.running {
+		errc <- errors.New("app is already running")
+		return errc
 	}
 
-	return o, nil
+	a.owner.OnError(func(err any) {
+		errc <- err
+	})
+
+	err := a.Render(parent, node)
+	if err != nil {
+		errc <- err
+		return errc
+	}
+
+	a.running = true
+	return errc
+}
+
+func (a *App) Render(parent string, node loom.Node) error {
+	err := a.owner.Run(func() error {
+		ctx := loom.NewRenderContext()
+		container := internal.Doc().Call("querySelector", parent)
+
+		return internal.RenderNodes(ctx, container, node)
+	})
+
+	if err != nil {
+		a.owner.Dispose()
+		return err
+	}
+
+	return nil
 }
